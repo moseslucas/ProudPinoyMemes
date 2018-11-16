1ed2c411ebfcb9a1ac09738856d57fec6f97a46d
moseslucas <mo@moseslucas.com>
2018-11-16 16:48:19 +0800

    Data in json file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,11 @@
 import React, { PureComponent } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 import { Container, Header, Body, Content, Title, Text, View } from 'native-base'
-import GridView from 'react-native-super-grid';
+import GridView from 'react-native-super-grid'
+import data from '../data/sounds.json'
 
 class Home extends PureComponent {
   render () { 
-    const items = [
-      { name: 'TURQUOISE', code: '#1abc9c' }, { name: 'EMERALD', code: '#2ecc71' },
-      { name: 'PETER RIVER', code: '#3498db' }, { name: 'AMETHYST', code: '#9b59b6' },
-      { name: 'WET ASPHALT', code: '#34495e' }, { name: 'GREEN SEA', code: '#16a085' },
-      { name: 'NEPHRITIS', code: '#27ae60' }, { name: 'BELIZE HOLE', code: '#2980b9' },
-      { name: 'WISTERIA', code: '#8e44ad' }, { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-      { name: 'SUN FLOWER', code: '#f1c40f' }, { name: 'CARROT', code: '#e67e22' },
-      { name: 'ALIZARIN', code: '#e74c3c' }, { name: 'CLOUDS', code: '#ecf0f1' },
-      { name: 'CONCRETE', code: '#95a5a6' }, { name: 'ORANGE', code: '#f39c12' },
-      { name: 'PUMPKIN', code: '#d35400' }, { name: 'POMEGRANATE', code: '#c0392b' },
-      { name: 'SILVER', code: '#bdc3c7' }, { name: 'ASBESTOS', code: '#7f8c8d' },
-    ]
     return (
       <Container>
         <Header>
@@ -27,13 +16,12 @@ class Home extends PureComponent {
         <Content>
           <GridView
             itemDimension={130}
-            items={items}
+            items={data}
             style={styles.gridView}
             renderItem={item => (
-              <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+              <TouchableOpacity style={[styles.itemContainer]}>
                 <Text style={styles.itemName}>{item.name}</Text>
-                <Text style={styles.itemCode}>{item.code}</Text>
-              </View>
+              </TouchableOpacity>
             )}
           />
         </Content>
@@ -51,6 +39,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     height: 150,
+    backgroundColor: '#ffc000'
   },
   itemName: {
     fontSize: 16,
