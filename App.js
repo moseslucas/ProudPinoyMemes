57a264807a66bab7b1bf06557b5880f6e6c594eb
moseslucas <mo@moseslucas.com>
2018-11-16 16:34:15 +0800

    Installed nativebase

---
+++ App.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+import Home from './src/components/Home'
+
+const App = () => {
+  return <Home />
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+import { Container, Header, Body, Content, Title, Text } from 'native-base'
+
+class Home extends PureComponent {
+  render () { 
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title> X </Title>
+          </Body>
+        </Header>
+        <Content>
+          <Text> X </Text>
+        </Content>
+      </Container>
+    )
+  }
+}
+
+export default Home
