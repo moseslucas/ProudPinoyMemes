e1bcfd73de5302bcb5c5c2cd5551cbcdb7c65879
moseslucas <mo@moseslucas.com>
2019-04-01 16:36:41 +0800

    Renamed app

--- ios/PeenoiseSoundBoardTests/PeenoiseSoundBoardTests.m
+++ ios/PeenoiseSoundBoardTests/PeenoiseSoundBoardTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface ProudPinoyMemesTests : XCTestCase
+@interface PeenoiseSoundBoardTests : XCTestCase
 
 @end
 
-@implementation ProudPinoyMemesTests
+@implementation PeenoiseSoundBoardTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
