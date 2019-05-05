392e233532472feffe9dff58f9d692d68456d414
moseslucas <mo@moseslucas.com>
2019-05-05 23:16:00 +0800

    AppCEnter codespuh

--- ios/PeenoiseSoundBoard/AppDelegate.m
+++ ios/PeenoiseSoundBoard/AppDelegate.m
@@ -6,6 +6,7 @@
  */
 
 #import "AppDelegate.h"
+#import <CodePush/CodePush.h>
 #import <AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h>
 #import <AppCenterReactNativeAnalytics/AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNative/AppCenterReactNative.h>
@@ -22,7 +23,12 @@
   [AppCenterReactNative register];  // Initialize AppCenter
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+                    #ifdef DEBUG
+                        jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+                    #else
+                        jsCodeLocation = [CodePush bundleURL];
+                    #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"PeenoiseSoundBoard"
